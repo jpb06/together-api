869e8c39d3bd72fb22d4f4897b66d8a021a8c202
jpb <jpb.06@outlook.com>
2019-09-23 19:47:04 +0200

    Config interface

---
+++ src/config/api.config.interface.ts
@@ -0,0 +1,19 @@
+﻿import * as ConfigData from './current.config.json';
+
+export interface ApiConfigInterface {
+    srvIPAddress: string,
+    expressListeningIPAddress: string,
+    srvURLs: string,
+    mongodbPort: number,
+    mainDb: string,
+    mainDbUsername: string,
+    mainDbPassword: string,
+    rsaVaultDb: string,
+    rsaVaultDbUsername: string,
+    rsaVaultDbPassword: string,
+    mongoAuthDb: string,
+}
+
+export function apiConfig(): ApiConfigInterface {
+    return <ApiConfigInterface><unknown>ConfigData;
+}
