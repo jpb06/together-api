8d7d9132c0707e74f0ed04a127c0a13a6f034571
jpb <jpb.06@outlook.com>
2019-09-23 19:47:19 +0200

    business -> Crypto utils

---
+++ src/business/util/crypto.util.ts
@@ -0,0 +1,21 @@
+﻿import * as bcrypt from 'bcrypt';
+
+export async function hash(
+    data: string
+): Promise<string> {
+    let salt = await bcrypt.genSalt(12);
+    let hash = await bcrypt.hash(data, salt);
+
+    return hash;
+}
+
+// used solely in api
+export async function verify(
+    data: string,
+    hash: string
+): Promise<boolean> {
+
+    let result = await bcrypt.compare(data, hash);
+
+    return result;
+}
