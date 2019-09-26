830d09bad73256eb0d864503cdb7fbd5ec795aeb
jpb <jpb.06@outlook.com>
2019-09-26 19:31:31 +0200

    Defining User, Team and Daily persisted types

--- src/dal/types/persisted.types.ts
+++ src/dal/types/persisted.types.ts
@@ -1,11 +1,40 @@
-﻿/* ---------------------------------------------------------------------------------------------------------------
+﻿import { ObjectId } from "bson";
+
+/* ---------------------------------------------------------------------------------------------------------------
    Persisted
    ---------------------------------------------------------------------------------------------------------------*/
 export class User {
+    _id: ObjectId;
+    teams: Array<Team>;
+
     email: string;
     password: string;
     session: Session;
-    teams: Array<Team>;
+
+    lastName: string;
+    firstName: string;
+    avatarName: string;
+}
+
+export class Team {
+    _id: ObjectId;
+
+    name: string;
+}
+
+export class Daily {
+    _id: ObjectId;
+    teamId: ObjectId;
+
+    day: number;
+    month: number;
+    year: number;
+
+    durationIndicator: string;
+    unforeseenTickets: Array<Ticket>;
+    doneTickets: Array<Ticket>;
+    issues: Array<string>;
+    feelings: Array<Feeling>;
 }
 
 /* ---------------------------------------------------------------------------------------------------------------
@@ -13,8 +42,25 @@ export class User {
    ---------------------------------------------------------------------------------------------------------------*/
 export class Session {
     expirationDate: string;
-
 }
-export class Team {
+
+export class Ticket {
+    assignee?: TerseUser;
+    creator: TerseUser;
+
     name: string;
 }
+
+export class Feeling {
+    subject: TerseUser;
+
+    type: number;
+    comment: string;
+}
+
+export class TerseUser {
+    lastName: string;
+    firstName: string;
+    avatarName: string;
+}
+
