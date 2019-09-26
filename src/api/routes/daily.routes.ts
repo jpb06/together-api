c81fa87cd06fa00fb769143719019b39dad3d689
jpb <jpb.06@outlook.com>
2019-09-26 19:35:30 +0200

    Daily routes

---
+++ src/api/routes/daily.routes.ts
@@ -0,0 +1,54 @@
+﻿import { Express, Request, Response } from "express-serve-static-core";
+import { isAuthenticated } from "../middleware/permissions.validation.middleware";
+import { DailyStore } from "../../dal/manipulation/stores/specific/daily.store";
+import { CacheService } from "../../business/cache.service";
+
+export function mapUserRoutes(app: Express) {
+
+    app.post('/api/daily/addunforeseen', isAuthenticated, async (
+        req: Request,
+        res: Response
+    ) => {
+        try {
+            if (!req.validateUnforeseen()) {
+                return res.answer(400, 'Expecting a ticket for a daily');
+            }
+
+            const creator = await CacheService.GetUser(res.locals.email);
+            if (creator) {
+                let result = await DailyStore.addUnforeseenTicket(req.body.date, req.body.teamId, creator, req.body.name);
+                if (result) {
+                    res.answer(201, `${req.body.name} created`);
+                } else {
+                    res.answer(500, `Unable to create ticket ${req.body.name}`);
+                }
+            } else {
+                res.answer(500, "Unable to retrieve ticket creator");
+            }
+        } catch (error) {
+            console.log(error);
+            return res.answer(500, error.message);
+        }
+    });
+
+    app.post('/api/daily/removeunforeseen', isAuthenticated, async (
+        req: Request,
+        res: Response
+    ) => {
+        try {
+            if (!req.validateUnforeseen()) {
+                return res.answer(400, 'Expecting a ticket for a daily');
+            }
+
+            let result = await DailyStore.removeUnforeseenTicket(req.body.date, req.body.teamId, req.body.name);
+            if (result) {
+                res.answer(200, `${req.body.name} deleted`);
+            } else {
+                res.answer(500, `Unable to delete ticket ${req.body.name}`);
+            }
+        } catch (error) {
+            console.log(error);
+            return res.answer(500, error.message);
+        }
+    });
+}
