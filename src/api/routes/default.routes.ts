3abc31ffeffd619de606850735b6f95aff905f7b
jpb <jpb.06@outlook.com>
2019-09-26 19:32:33 +0200

    Moving default routes to their own definition file

---
+++ src/api/routes/default.routes.ts
@@ -0,0 +1,10 @@
+﻿import { Express, Request, Response } from "express-serve-static-core";
+
+export function mapDefaultRoutes(app: Express) {
+    app.get('/', (req, res) => {
+        res.send('Valid routes are in /api');
+    });
+    app.get('/api/', (req, res) => {
+        res.send('Together api.');
+    });
+};
