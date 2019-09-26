d380d3fd5e02090fd9d74820e2fe8c68dcb030da
jpb <jpb.06@outlook.com>
2019-09-26 19:34:23 +0200

    Teams store : create/get

---
+++ src/dal/manipulation/stores/specific/teams.store.ts
@@ -0,0 +1,31 @@
+﻿import { GenericStore } from '../dal.generic.store';
+import { Team } from '../../../types/persisted.types';
+import { ObjectId } from 'bson';
+
+export abstract class TeamsStore {
+    public static storeName = 'teams';
+
+    public static async create(
+        name: string
+    ): Promise<ObjectId | undefined> {
+
+        const insertedId = await GenericStore.create(this.storeName, {
+            name: name
+        });
+        return insertedId;
+    }
+
+    public static async get(
+        id: ObjectId
+    ): Promise<Team | undefined> {
+
+        const result = await GenericStore.getBy(
+            this.storeName,
+            { _id: id },
+            {}) as Array<Team>;
+
+        if (result.length !== 1) return undefined;
+
+        return result[0];
+    }
+}
