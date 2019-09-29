d15ef70d9ed5a562baf8ea20bf3f2f59c07fb3dc
jpb <jpb.06@outlook.com>
2019-09-29 12:49:09 +0200

    Types for api requests content validation

---
+++ src/dal/types/internal.types.ts
@@ -0,0 +1,14 @@
+﻿import { ObjectId } from "bson";
+
+/* ---------------------------------------------------------------------------------------------------------------
+   Api
+   ---------------------------------------------------------------------------------------------------------------*/
+export class UnforeseenData {
+    teamId: ObjectId;
+    date: Date;
+    ticketName: string;
+}
+export class DailyPredicate {
+    teamId: ObjectId;
+    date: Date;
+}
