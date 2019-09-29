44a310cbb7636287a454449f7e34162998c981df
jpb <jpb.06@outlook.com>
2019-09-29 12:47:21 +0200

    Util for types validation in api request calls

---
+++ src/business/util/validation.util.ts
@@ -0,0 +1,12 @@
+﻿export function isDateValid(
+    date: Date
+): boolean {
+    return (date.toString() !== 'Invalid Date');
+}
+
+export function containsHex(
+    data: string,
+    expectedLength: number
+) {
+    return data.match('^[0-9A-Fa-f]{' + expectedLength + '}$');
+}
