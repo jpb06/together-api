5a853f451bf9e4ab2afcae3e18ffd341cfb48541
jpb <jpb.06@outlook.com>
2019-09-29 12:52:13 +0200

    Using improved request validation for daily route

--- src/api/routes/daily.routes.ts
+++ src/api/routes/daily.routes.ts
@@ -3,51 +3,69 @@ import { isAuthenticated } from "../middleware/permissions.validation.middleware
 import { DailyStore } from "../../dal/manipulation/stores/specific/daily.store";
 import { CacheService } from "../../business/cache.service";
 
-export function mapUserRoutes(app: Express) {
+export function mapDailyRoutes(app: Express) {
 
-    app.post('/api/daily/addunforeseen', isAuthenticated, async (
+    app.post('/api/daily/unforeseen/add', isAuthenticated, async (
         req: Request,
         res: Response
     ) => {
         try {
-            if (!req.validateUnforeseen()) {
+            const data = req.validateUnforeseen();
+            if (!data) {
                 return res.answer(400, 'Expecting a ticket for a daily');
             }
 
             const creator = await CacheService.GetUser(res.locals.email);
             if (creator) {
-                let result = await DailyStore.addUnforeseenTicket(req.body.date, req.body.teamId, creator, req.body.name);
+                let result = await DailyStore.addUnforeseenTicket(data.date, data.teamId, creator, data.ticketName);
                 if (result) {
-                    res.answer(201, `${req.body.name} created`);
+                    res.answer(201, `${data.ticketName} created`);
                 } else {
-                    res.answer(500, `Unable to create ticket ${req.body.name}`);
+                    res.answer(500, `Unable to create ticket ${data.ticketName}`);
                 }
             } else {
                 res.answer(500, "Unable to retrieve ticket creator");
             }
         } catch (error) {
-            console.log(error);
             return res.answer(500, error.message);
         }
     });
 
-    app.post('/api/daily/removeunforeseen', isAuthenticated, async (
+    app.post('/api/daily/unforeseen/remove', isAuthenticated, async (
         req: Request,
         res: Response
     ) => {
         try {
-            if (!req.validateUnforeseen()) {
+            const data = req.validateUnforeseen();
+            if (!data) {
                 return res.answer(400, 'Expecting a ticket for a daily');
             }
 
-            let result = await DailyStore.removeUnforeseenTicket(req.body.date, req.body.teamId, req.body.name);
+            let result = await DailyStore.removeUnforeseenTicket(data.date, data.teamId, data.ticketName);
             if (result) {
-                res.answer(200, `${req.body.name} deleted`);
+                res.answer(200, `${data.ticketName} deleted`);
             } else {
-                res.answer(500, `Unable to delete ticket ${req.body.name}`);
+                res.answer(500, `Unable to delete ticket ${data.ticketName}`);
             }
         } catch (error) {
-            console.log(error);
+            return res.answer(500, error.message);
+        }
+    });
+
+    app.post('/api/daily', isAuthenticated, async (
+        req: Request,
+        res: Response
+    ) => {
+        try {
+            const data = req.validateDailyPredicate();
+            if (!data) {
+                return res.answer(400, 'Expecting a daily predicate');
+            }
+
+            let result = await DailyStore.getCreateDaily(data.date, data.teamId);
+            res.populate(result);
+
+        } catch (error) {
             return res.answer(500, error.message);
         }
     });
