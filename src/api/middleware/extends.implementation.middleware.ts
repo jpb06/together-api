7818ebcf1a383bd8553f87c55be00ea51e83fdd5
jpb <jpb.06@outlook.com>
2019-10-06 12:17:35 +0200

    Ditto for request extends implementation

--- src/api/middleware/extends.implementation.middleware.ts
+++ src/api/middleware/extends.implementation.middleware.ts
@@ -1,76 +1,10 @@
 ﻿import { Request, Response, NextFunction } from 'express';
-import { ObjectId } from 'bson';
-import { UnforeseenData, DailyPredicate } from '../../dal/types/internal.types';
-import { isDateValid, containsHex } from '../../business/util/validation.util';
 
 export function extendsImplementation(
     req: Request,
     res: Response,
     next: NextFunction
 ) {
-    // ----------------------------------------------------------------------------------------------
-    // Request extends
-    // ----------------------------------------------------------------------------------------------
-    req.validateLogin = function (): boolean {
-        if ((req.body.login === undefined || req.body.login === '') ||
-            (req.body.password === undefined || req.body.password === '')) {
-            return false;
-        }
-
-        return true;
-    }
-
-    req.validateUnforeseen = function (): UnforeseenData | undefined {
-        if ((req.body.ticket === undefined || req.body.ticket === '') ||
-            (req.body.date === undefined || req.body.date === '') ||
-            (req.body.teamId === undefined || req.body.teamId === '')) {
-            return undefined;
-        }
-
-        const date = new Date(req.body.date);
-
-        if (!isDateValid(date)) return undefined;
-        if (!containsHex(req.body.teamId, 24)) {
-            return undefined
-        };
-
-        const teamId = ObjectId.createFromHexString(req.body.teamId);
-
-        return {
-            ticketName: req.body.ticket,
-            teamId: teamId,
-            date: date
-        };
-    }
-
-    req.validateDailyPredicate = function (): DailyPredicate | undefined {
-        if ((req.body.date === undefined || req.body.date === '') ||
-            (req.body.teamId === undefined || req.body.teamId === '')) {
-            return undefined;
-        }
-
-        const date = new Date(req.body.date);
-
-        if (!isDateValid(date)) return undefined;
-        if (!containsHex(req.body.teamId, 24)) {
-            return undefined
-        };
-
-        const teamId = ObjectId.createFromHexString(req.body.teamId);
-
-        return {
-            teamId: teamId,
-            date: date
-        };
-    }
-
-    req.validateUser = function (): boolean {
-        if ((req.body.user === undefined || req.body.user === '')) {
-            return false;
-        }
-
-        return true;
-    }
     // ----------------------------------------------------------------------------------------------
     // Response extends
     // ----------------------------------------------------------------------------------------------
