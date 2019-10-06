cd81f19bead315487ec1122f2a68b3c527262ba1
jpb <jpb.06@outlook.com>
2019-10-06 12:17:13 +0200

    Getting rid of request extends; keeping response extends for now.

--- src/api/extends/request.extends.ts
+++
@@ -1,9 +0,0 @@
-﻿declare namespace Express {
-    export interface Request {
-        validateLogin: () => boolean;
-
-        validateUnforeseen: () => import("./../../dal/types/internal.types").UnforeseenData | undefined;
-        validateDailyPredicate: () => import("./../../dal/types/internal.types").DailyPredicate | undefined;
-        validateUser: () => boolean;
-    }
-}
