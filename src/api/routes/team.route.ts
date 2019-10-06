1807b3ad3056ad7ae87b838dd1309674f9724a6b
jpb <jpb.06@outlook.com>
2019-10-06 12:19:09 +0200

    Adding a route to get team members

---
+++ src/api/routes/team.route.ts
@@ -0,0 +1,21 @@
+﻿import { Express, Request, Response } from "express-serve-static-core";
+import { isAuthenticated } from "../middleware/permissions.validation.middleware";
+import { containsTeamId } from "../middleware/requests.validation.middleware";
+import { UsersStore } from "../../dal/manipulation/stores/specific/users.store";
+
+export function mapTeamRoutes(app: Express) {
+
+    app.post('/api/team/members', isAuthenticated, containsTeamId, async (
+        req: Request,
+        res: Response
+    ) => {
+        try {
+            const users = await UsersStore.GetTeamMembers(res.locals.teamId);
+
+            res.populate(users);
+        } catch (error) {
+            console.log(error);
+            return res.answer(500, error.message);
+        }
+    });
+}
