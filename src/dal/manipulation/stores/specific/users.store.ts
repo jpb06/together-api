d6203bc44c79359c2db19adb2933fb9851b420bf
jpb <jpb.06@outlook.com>
2019-10-19 09:31:56 +0200

    Getting user by id (cache related)

--- src/dal/manipulation/stores/specific/users.store.ts
+++ src/dal/manipulation/stores/specific/users.store.ts
@@ -3,6 +3,7 @@ import { User, TerseUser } from '../../../types/persisted.types';
 import * as Crypto from './../../../../business/util/crypto.util';
 import { TeamsStore } from './teams.store';
 import { ObjectId } from 'bson';
+import { CacheService } from '../../../../business/cache.service';
 
 export abstract class UsersStore {
     public static storeName = 'users';
@@ -40,7 +41,7 @@ export abstract class UsersStore {
         teamId: ObjectId
     ): Promise<boolean> {
 
-        let user = await this.get(email);
+        let user = await CacheService.GetUserByEmail(email);
         if (user && user.teams.filter(el => el._id.equals(teamId)).length === 0) {
             const team = await TeamsStore.get(teamId);
             if (team) {
@@ -58,7 +59,22 @@ export abstract class UsersStore {
         else return false;
     }
 
-    public static async get(
+    public static async getById(
+        id: ObjectId
+    ): Promise<User | undefined> {
+
+        const result = await GenericStore.getBy(
+            this.storeName,
+            { _id: id },
+            {}
+        ) as Array<User>;
+
+        if (result.length !== 1) return undefined;
+
+        return result[0];
+    }
+
+    public static async getByEmail(
         email: string
     ): Promise<User | undefined> {
 
@@ -90,7 +106,7 @@ export abstract class UsersStore {
         teamId: ObjectId
     ): Promise<Array<TerseUser>> {
 
-        let users = await GenericStore.getBy(
+        const users = await GenericStore.getBy(
             this.storeName,
             { 'teams': { $elemMatch: { _id: teamId } } },
             {}
