d82d6f8ea4dc947b9aa67df0b62cc12fc211ef94
jpb <jpb.06@outlook.com>
2019-10-19 09:32:34 +0200

    Adding a constant timeout on api responses so that transitions can always be seen

--- src/togetherapi.ts
+++ src/togetherapi.ts
@@ -28,6 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(extendsImplementation);
+app.use((req, res, next) => setTimeout(next, 1500));
 
 mapDefaultRoutes(app);
 mapSecurityRoutes(app);
