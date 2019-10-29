1cca17a8380b19e8a857a8a1a0fe8bc12aa8a111
jpb <jpb.06@outlook.com>
2019-10-29 18:33:20 +0100

    Adding a route to get the teams a user is part of

--- src/api/routes/user.routes.ts
+++ src/api/routes/user.routes.ts
@@ -1,15 +1,25 @@
 ﻿import { Express, Request, Response } from "express-serve-static-core";
 import { isAuthenticated } from "../middleware/permissions.validation.middleware";
+import { containsUserId } from "../middleware/requests.validation.middleware";
+import { ObjectId } from "bson";
+import { TeamsStore } from "../../dal/manipulation/stores/specific/teams.store";
 
 export function mapUserRoutes(app: Express) {
 
-    app.post('/api/users/get', isAuthenticated, async (
+    app.post('/api/users/teams', isAuthenticated, containsUserId, async (
         req: Request,
         res: Response
     ) => {
         try {
 
+            const userId = <ObjectId>res.locals.userId;
 
+            const userTeams = await TeamsStore.getUserTeams(userId);
+            if (userTeams) {
+                res.populate(userTeams);
+            } else {
+                res.answer(500, 'Unable to get user teams');
+            }
         } catch (error) {
             console.log(error);
             return res.answer(500, error.message);
