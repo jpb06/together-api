67123122c07614c4ff07abd21a3ebaf2dd12cc42
jpb <jpb.06@outlook.com>
2019-11-04 19:48:58 +0100

    Adding a quick test route to check db access

--- src/api/routes/default.routes.ts
+++ src/api/routes/default.routes.ts
@@ -1,4 +1,5 @@
 ﻿import { Express, Request, Response } from "express-serve-static-core";
+import { UsersStore } from "../../dal/manipulation/stores/specific/users.store";
 
 export function mapDefaultRoutes(app: Express) {
     app.get('/', (req, res) => {
@@ -6,5 +7,17 @@ export function mapDefaultRoutes(app: Express) {
     });
     app.get('/api/', (req, res) => {
         res.send('Together api.');
+    });
+    app.get('/api/test', async (req, res) => {
+        try {
+            const user = await UsersStore.getByEmail('jpb.06@outlook.fr');
+            if (user) {
+                res.populate(user.lastName);
+            } else {
+                res.send('Unable to find user');
+            }
+        } catch (error) {
+            return res.answer(500, error.message);
+        }
     });
 };
