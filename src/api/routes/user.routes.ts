1e58d34725afc844e56bd62e49e5ecfc5fffdf5a
jpb <jpb.06@outlook.com>
2019-11-15 18:34:43 +0100

    User creation route

--- src/api/routes/user.routes.ts
+++ src/api/routes/user.routes.ts
@@ -1,13 +1,60 @@
 ﻿import { Express, Request, Response } from "express-serve-static-core";
 import { isAuthenticated } from "../middleware/permissions.validation.middleware";
-import { containsUserId } from "../middleware/requests.validation.middleware";
+import { containsUserId, containsNewUser } from "../middleware/requests.validation.middleware";
 import { ObjectId } from "bson";
 import { TeamsStore } from "../../dal/manipulation/stores/specific/teams.store";
 import { DailyStore } from "../../dal/manipulation/stores/specific/daily.store";
-import { TimeLineEntry } from "../../dal/types/internal.types";
+import { TimeLineEntry, NewUserData } from "../../dal/types/internal.types";
 import moment = require("moment");
+import { UsersStore } from "../../dal/manipulation/stores/specific/users.store";
+import { CacheService } from "../../business/cache.service";
 
 export function mapUserRoutes(app: Express) {
+
+    app.post('/api/user/create', containsNewUser, async (
+        req: Request,
+        res: Response
+    ) => {
+        try {
+            const newUser = <NewUserData>res.locals.newUser;
+
+            const existingUser = await CacheService.GetUserByEmail(newUser.email);
+            if (existingUser) {
+                return res.status(400).json({
+                    status: 400,
+                    error: 'Mail already used'
+                });
+            } else {
+
+                const persistedUser = await UsersStore.createUsingPassword(
+                    newUser.email,
+                    newUser.lastName, newUser.firstName,
+                    '', // no avatar for now
+                    newUser.password);
+
+                if (persistedUser) {
+                    return res.status(200).json({
+                        status: 200,
+                        user: {
+                            id: persistedUser._id,
+                            email: persistedUser.email,
+                            lastName: persistedUser.lastName,
+                            firstName: persistedUser.firstName,
+                            avatarName: persistedUser.avatarName,
+                        }
+                    });
+                } else {
+                    res.answer(500, 'Unable to create user');
+                }
+            }
+
+
+
+        } catch (error) {
+            console.log(error);
+            return res.answer(500, error.message);
+        }
+    });
 
     app.post('/api/user/teams', isAuthenticated, containsUserId, async (
         req: Request,
