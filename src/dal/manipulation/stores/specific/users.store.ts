16e4afd44cdf6ce0c069737b5380623ef7f562f4
jpb <jpb.06@outlook.com>
2019-11-26 19:23:26 +0100

    Getting rid of createUsingPassword (no longer useful) and adding an empty membershipRequests array to new users

--- src/dal/manipulation/stores/specific/users.store.ts
+++ src/dal/manipulation/stores/specific/users.store.ts
@@ -1,5 +1,5 @@
 ﻿import { GenericStore } from '../dal.generic.store';
-import { User } from '../../../types/persisted.types';
+import { User, TerseUser } from '../../../types/persisted.types';
 import * as Crypto from './../../../../business/util/crypto.util';
 import { TeamsStore } from './teams.store';
 import { ObjectId } from 'bson';
@@ -9,34 +9,6 @@ export abstract class UsersStore {
     public static storeName = 'users';
 
     public static async create(
-        email: string,
-        lastname: string,
-        firstname: string,
-        avatarName: string
-    ): Promise<string> {
-
-        const password: string = [...Array(8)].map(i => (~~(Math.random() * 36)).toString(36)).join('');
-
-        const hash = await Crypto.hash(password);
-
-        await GenericStore.createOrUpdate(
-            this.storeName,
-            { email: email },
-            {
-                email: email,
-                password: hash,
-                lastname: lastname,
-                firstname: firstname,
-                avatarName: avatarName,
-                teams: [],
-                session: undefined
-            }
-        );
-
-        return password;
-    }
-
-    public static async createUsingPassword(
         email: string,
         lastName: string,
         firstName: string,
@@ -56,6 +28,7 @@ export abstract class UsersStore {
                 firstName: firstName,
                 avatarName: avatarName,
                 teams: [],
+                membershipRequests: [],
                 session: undefined
             }
         );
