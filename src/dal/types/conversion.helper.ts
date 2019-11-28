f5a4a32a84b09d2e7663f9abaa854f1f2866adbe
jpb <jpb.06@outlook.com>
2019-11-28 18:42:04 +0100

    Adding a helper to convert a team into a bare team

--- src/dal/types/conversion.helper.ts
+++ src/dal/types/conversion.helper.ts
@@ -1,14 +1,23 @@
-import { TerseUser, User } from "./persisted.types";
+import { TerseUser, User, BareTeam, Team } from "./persisted.types";
 
 const userToTerseUser = (
     user: User
 ): TerseUser => ({
-    _id: user._id,
-    lastName: user.lastName,
-    firstName: user.firstName,
-    avatarName: user.avatarName
+        _id: user._id,
+        lastName: user.lastName,
+        firstName: user.firstName,
+        avatarName: user.avatarName
+    });
+
+const TeamToBareTeam = (
+    team: Team
+): BareTeam => ({
+        _id: team._id,
+        name: team.name
 });
 
+
 export {
-    userToTerseUser
+    userToTerseUser,
+    TeamToBareTeam
 };
